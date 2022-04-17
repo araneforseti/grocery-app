10b7dc403c87d339f7e6fc871d88a25642b0a789
Sarah Hutchins <974016+araneforseti@users.noreply.github.com>
2022-04-17 11:23:57 +0200

    Navigation to Groceries view

---
+++ app/src/main/java/com/semblanceoffunctionality/groceryapp/ui/grocery/GroceryFragment.kt
@@ -0,0 +1,36 @@
+package com.semblanceoffunctionality.groceryapp.ui.grocery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.semblanceoffunctionality.groceryapp.databinding.FragmentGroceryBinding
+
+class GroceryFragment : Fragment() {
+
+    private var _binding: FragmentGroceryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val groceryViewModel =
+            ViewModelProvider(this).get(GroceryViewModel::class.java)
+
+        _binding = FragmentGroceryBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
