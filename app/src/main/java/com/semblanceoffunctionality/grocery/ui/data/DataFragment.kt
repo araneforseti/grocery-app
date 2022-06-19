859118491ad2bde46e3351c15b5d8852b9a19d77
Sarah Hutchins <974016+araneforseti@users.noreply.github.com>
2022-06-19 23:01:15 +0200

    Export data working!

--- app/src/main/java/com/semblanceoffunctionality/grocery/ui/data/DataFragment.kt
+++ app/src/main/java/com/semblanceoffunctionality/grocery/ui/data/DataFragment.kt
@@ -1,26 +1,32 @@
 package com.semblanceoffunctionality.grocery.ui.data
 
-import android.Manifest
-import android.content.DialogInterface
-import android.os.Build
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
 import com.semblanceoffunctionality.grocery.R
-import com.semblanceoffunctionality.grocery.data.Store
 import com.semblanceoffunctionality.grocery.databinding.FragmentDataBinding
-import kotlinx.coroutines.NonCancellable.start
+import com.semblanceoffunctionality.grocery.ui.data.DataFragment.WorkerCallback
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class DataFragment : Fragment()  {
     private val dataViewModel: DataViewModel by viewModels()
+    private lateinit var startForResult: ActivityResultLauncher<Intent?>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,62 +42,54 @@ class DataFragment : Fragment()  {
         ).apply {
             lifecycleOwner = viewLifecycleOwner
             exportCallback = WorkerCallback {
-                Log.e("export", "export")
-                if(isStoragePermissionGranted()) {
-                    val builder = AlertDialog.Builder(requireContext())
-                    builder.setMessage("Export not yet implemented")
-                        .setPositiveButton(R.string.ok,
-                            DialogInterface.OnClickListener { dialog, id ->
-                            })
-                    val dialog = builder.create()
-                    dialog.show()
-                } else {
-                    alertPermission()
-                }
+                writeToStorage()
+
+                Snackbar.make(root, R.string.data_exported, Snackbar.LENGTH_LONG)
+                    .show()
             }
             importCallback = WorkerCallback {
                 Log.e("import", "import")
-                if(isStoragePermissionGranted()) {
-                    val builder = AlertDialog.Builder(requireContext())
-                    builder.setMessage("Import not yet implemented")
-                        .setPositiveButton(R.string.ok,
-                            DialogInterface.OnClickListener { dialog, id ->
-                            })
-                    val dialog = builder.create()
-                    dialog.show()
-                } else {
-                    alertPermission()
-                }
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setMessage("Import not yet implemented")
+                    .setPositiveButton(R.string.ok
+                    ) { _, _ ->
+                    }
+                val dialog = builder.create()
+                dialog.show()
             }
         }
 
         return binding.root
     }
 
-    private fun alertPermission() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setMessage(R.string.no_permission)
-            .setPositiveButton(R.string.ok,
-                DialogInterface.OnClickListener { dialog, id ->
-                })
-        val dialog = builder.create()
-        dialog.show()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val jsonContent = dataViewModel.exportData()
+                val chosenUri = result.data?.data!!
+                val outputStream = context?.contentResolver?.openOutputStream(chosenUri)
+                outputStream?.write(jsonContent?.toByteArray())
+                outputStream?.close()
+            }
+        }
     }
 
-    private fun isStoragePermissionGranted(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (PermissionChecker.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PermissionChecker.PERMISSION_GRANTED) {
-                //Permission is granted
-                true
-            } else {
-                //Permission is revoked
-                ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-                false
-            }
-        } else {
-            // Permission is automatically granted on sdk<23 upon installation
-            true
+    private fun writeToStorage() {
+        val relativePath = "Documents/"
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/json"
+            putExtra(Intent.EXTRA_TITLE, "forsetiGroceryBackup.json")
+
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, Uri.parse(relativePath))
         }
+
+        startForResult.launch(intent)
     }
 
     fun interface WorkerCallback {
--- app/src/main/java/com/semblanceoffunctionality/grocery/ui/data/DataViewModel.kt
+++ app/src/main/java/com/semblanceoffunctionality/grocery/ui/data/DataViewModel.kt
@@ -1,25 +1,26 @@
 package com.semblanceoffunctionality.grocery.ui.data
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.semblanceoffunctionality.grocery.data.ItemRepository
 import com.semblanceoffunctionality.grocery.data.StockStatusRepository
 import com.semblanceoffunctionality.grocery.data.StoreRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
+/**
+ * The ViewModel used in [DataFragment].
+ */
+@HiltViewModel
 class DataViewModel @Inject constructor(
     private val storeRepository: StoreRepository,
     private val itemRepository: ItemRepository,
     private val stockStatusRepository: StockStatusRepository
 )  : ViewModel() {
+
     private var gson: Gson? = null
 
     fun exportData() = runBlocking {
